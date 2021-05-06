6fa668e1231f0b6d38738763542082ddce3a3bb7
denisd456 <denis.durasinovic@gmail.com>
2021-05-06 13:17:24 +0200

    Initial commit Pytorch 1.6.0, cuda 10.1 Simple baselines for human pose estimation from Microsoft Dockerfile includes all dependencies

---
+++ dockerfile/first_run_cont.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash 
+
+# export DATA_PATH=$PWD in correct folder /home/Work/data
+#docker run -it --network host --privileged --gpus all \
+#       -e DISPLAY=$DISPLAY \
+#       -v /dev:/dev \
+#       -v /tmp/.X11-unix:/tmp/.X11-unix \
+#       -v $DATA_PATH:/home/developer/data \
+#       --name hpe_cont hpe
+
+# Kod -v staviti put do models i data direktorija koji su instalirani prema github instrukcijama
+sudo docker run -it --network host --privileged --gpus all -e DISPLAY=$DISPLAY -v /mnt/Data/LINUX/docker/models:/home/developer/human-pose-estimation.pytorch/models -v /mnt/Data/LINUX/docker/data:/home/developer/human-pose-estimation.pytorch/data --shm-size 12G --name hpe_1 hpe
+
+
