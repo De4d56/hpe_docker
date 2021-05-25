65f3a599ecd4995b52bf5314d80fac2dcb1b90c5
De4d56 <65813158+De4d56@users.noreply.github.com>
2021-05-25 17:38:43 +0200

    Updated dependencies, ROS

--- dockerfile/first_run_cont.sh
+++ dockerfile/first_run_cont.sh
@@ -9,6 +9,6 @@
 #       --name hpe_cont hpe
 
 # Kod -v staviti put do models i data direktorija koji su instalirani prema github instrukcijama
-sudo docker run -it --network host --privileged --gpus all -e DISPLAY=$DISPLAY -v /media/denis/Data/LINUX/docker/models:/home/developer/human-pose-estimation.pytorch/models -v /media/denis/Data/LINUX/docker/data:/home/developer/human-pose-estimation.pytorch/data --shm-size 12G --name hpe_1 hpe
+sudo docker run -it --network host --privileged --gpus all -e DISPLAY=$DISPLAY -v /media/denis/Data/LINUX/docker/models:/home/developer/hpe_ros/models -v /media/denis/Data/LINUX/docker/data:/home/developer/hpe_ros/data -v /dev:/dev  --shm-size 12G --name hpe_1 hpe
 
 
