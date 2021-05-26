b2e61a5bc52796b553f426c4e59d723ab683a5fe
De4d56 <65813158+De4d56@users.noreply.github.com>
2021-05-26 23:54:11 +0200

    Fixed ROS and directories, automatically downloads models

--- dockerfile/first_run_cont.sh
+++ dockerfile/first_run_cont.sh
@@ -9,6 +9,6 @@
 #       --name hpe_cont hpe
 
 # Kod -v staviti put do models i data direktorija koji su instalirani prema github instrukcijama
-sudo docker run -it --network host --privileged --gpus all -e DISPLAY=$DISPLAY -v /media/denis/Data/LINUX/docker/models:/home/developer/hpe_ros/models -v /media/denis/Data/LINUX/docker/data:/home/developer/hpe_ros/data -v /dev:/dev  --shm-size 12G --name hpe_1 hpe
+sudo docker run -it --network host --privileged --gpus all -e DISPLAY=$DISPLAY -v /media/denis/Data/LINUX/docker/models:/home/developer/catkin_ws/src/hpe/include_files/models -v /media/denis/Data/LINUX/docker/data:/home/developer/catkin_ws/src/hpe/include_files/data -v /dev:/dev  --shm-size 12G --name hpe_1 hpe
 
 
